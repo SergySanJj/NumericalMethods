cb8ec554a6b235eff930e0339add01d0b357d53f
Sergei Yarema <isara.isara8@gmail.com>
2019-10-02 12:05:55 +0200

    Add newton's method

--- relaxation.py
+++ relaxation.py
@@ -29,9 +29,35 @@ def relaxation(f, fDX, x_left: float, x_right: float, x_start: float, eps: float
     estimated_num_iteration = int(
         np.ceil(np.log(np.abs(x_start - x_i) / eps) / np.log(1. / q))) + 1
 
-    print('Relaxtion method. Apriori iteration number: {0}, aposteriori iteration number: {1}'.format(
+    print('Relaxtion method. \nApriori iteration number: {0}, aposteriori iteration number: {1}'.format(
         estimated_num_iteration, num_iteration))
 
+    return x_i
+
+
+def newton(f, fDX, x_left: float, x_right: float, x_start: float, eps: float = 1e-4) -> float:
+    boundary_check(x_start, x_left, x_right)
+
+    num_iteration = 0
+    x_values = np.linspace(x_left, x_right, min(int(1. / eps), 10 ** 5))
+
+    min_value, max_value = np.min(
+        np.abs(fDX(x_values))), np.max(np.abs(fDX(x_values)))
+
+    q = (max_value - min_value) / (min_value + max_value)
+
+    x_i, x_prev = x_start, x_start - 2 * eps
+    num_iteration = 0
+
+    while abs(x_i - x_prev) >= eps:
+        x_i, x_prev = x_i - f(x_i)/fDX(x_i) , x_i
+        num_iteration += 1
+
+    # estimated_num_iteration = int(
+    #     np.ceil(np.log(np.log(np.abs(x_start - x_i) / 2.) / np.log(1. / q) + 1))) + 1
+
+    # print('Newton\'s method. \nApriori iteration number: {0}, aposteriori iteration number: {1}'.format(
+    #     estimated_num_iteration, num_iteration))
     return x_i
 
 
@@ -44,9 +70,8 @@ def main():
     f = lambdify(x, function, 'numpy')
     fDX = lambdify(x, functionDX, 'numpy')
 
-    print(relaxation(f,fDX,0,1,0))
-
-    
+    print(relaxation(f, fDX, 0, 1, 0))
+    print(newton(f, fDX, 0, 1, 0))
 
 
 main()
